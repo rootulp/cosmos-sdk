dba1a15550de1c8aa73deb93f2eefa7a9dd72a93
Rootul Patel <rootulp@gmail.com>
2025-05-05 21:55:51 -0400

    refactor: units

--- x/gov/types/v1/params.go
+++ x/gov/types/v1/params.go
@@ -18,8 +18,9 @@ const (
 
 // Default governance params
 var (
-	DefaultMinDepositTokens          = sdkmath.NewInt(10_000_000_000)                                                     // 10,000 TIA
-	DefaultMinExpeditedDepositTokens = DefaultMinDepositTokens.Mul(sdkmath.NewInt(DefaultMinExpeditedDepositTokensRatio)) // 50,000 TIA
+	tia                              = int64(1_000_000)             // 1 TIA = 1,000,000 utia
+	DefaultMinDepositTokens          = sdkmath.NewInt(10_000 * tia) // 10,000 TIA
+	DefaultMinExpeditedDepositTokens = sdkmath.NewInt(50_000 * tia) // 50,000 TIA
 	DefaultQuorum                    = sdkmath.LegacyNewDecWithPrec(334, 3)
 	DefaultThreshold                 = sdkmath.LegacyNewDecWithPrec(5, 1)
 	DefaultExpeditedThreshold        = sdkmath.LegacyNewDecWithPrec(667, 3)
