eb64a6cb9ca725de16737c1639a442c4cd8841b3
Rootul Patel <rootulp@gmail.com>
2025-05-05 21:59:41 -0400

    test: move assertions

--- x/gov/migrations/v5/store_test.go
+++ x/gov/migrations/v5/store_test.go
@@ -45,11 +45,12 @@ func TestMigrateStore(t *testing.T) {
 	require.NoError(t, cdc.Unmarshal(bz, &params))
 	require.NotNil(t, params)
 	require.Equal(t, v1.DefaultParams().ExpeditedMinDeposit, params.ExpeditedMinDeposit)
-	require.Equal(t, "utia", params.ExpeditedMinDeposit[0].Denom)
-	require.Equal(t, math.NewInt(50_000_000_000), params.ExpeditedMinDeposit[0].Amount) // 50,000 TIA
 	require.Equal(t, v1.DefaultParams().ExpeditedThreshold, params.ExpeditedThreshold)
 	require.Equal(t, v1.DefaultParams().ExpeditedVotingPeriod, params.ExpeditedVotingPeriod)
 	require.Equal(t, v1.DefaultParams().MinDepositRatio, params.MinDepositRatio)
+
+	require.Equal(t, "utia", params.ExpeditedMinDeposit[0].Denom)
+	require.Equal(t, math.NewInt(50_000_000_000), params.ExpeditedMinDeposit[0].Amount) // 50,000 TIA
 
 	// Check constitution
 	result, err := constitutionCollection.Get(ctx)
