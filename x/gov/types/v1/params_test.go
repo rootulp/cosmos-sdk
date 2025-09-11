cdc029a2479a8931d0a946fc792a4190b1e47830
Rootul Patel <rootulp@gmail.com>
2025-09-11 16:41:10 -0400

    fix: more test cases

--- x/gov/types/v1/params_test.go
+++ x/gov/types/v1/params_test.go
@@ -2,21 +2,35 @@ package v1
 
 import (
 	"testing"
+	time "time"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestValidateBasic(t *testing.T) {
-	t.Run("default params should pass ValidateBasic", func(t *testing.T) {
+	t.Run("default params should pass", func(t *testing.T) {
 		params := DefaultParams()
 		err := params.ValidateBasic()
 		require.NoError(t, err)
 	})
 
-	t.Run("params with expedited voting period equal to voting period should pass ValidateBasic", func(t *testing.T) {
+	t.Run("params with voting period == expedited voting period should pass", func(t *testing.T) {
 		params := DefaultParams()
 		params.ExpeditedVotingPeriod = params.VotingPeriod
 		err := params.ValidateBasic()
 		require.NoError(t, err)
+	})
+
+	t.Run("params with voting period less than expedited voting period should fail", func(t *testing.T) {
+		params := DefaultParams()
+
+		oneHour := time.Hour
+		params.VotingPeriod = &oneHour
+
+		twoHours := 2 * time.Hour
+		params.ExpeditedVotingPeriod = &twoHours
+
+		err := params.ValidateBasic()
+		require.Error(t, err)
 	})
 }
