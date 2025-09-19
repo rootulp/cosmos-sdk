090a942fcc8287e691f04371812318386c91fc26
Rootul Patel <rootulp@gmail.com>
2025-09-19 15:14:10 -0400

    debug(iavl): ignore error
    Inspired by https://github.com/reecepbcups/cosmos-sdk/commit/a004ac4dce5b8ee93db6dfd90672cd3ef64e6b9b

--- store/iavl/store.go
+++ store/iavl/store.go
@@ -104,7 +104,7 @@ func UnsafeNewStore(tree *iavl.MutableTree) *Store {
 // Any mutable operations executed will result in a panic.
 func (st *Store) GetImmutable(version int64) (*Store, error) {
 	if !st.VersionExists(version) {
-		return nil, errors.New("version mismatch on immutable IAVL tree; version does not exist. Version has either been pruned, or is for a future block height")
+		return &Store{tree: &immutableTree{&iavl.ImmutableTree{}}}, nil
 	}
 
 	iTree, err := st.tree.GetImmutable(version)
