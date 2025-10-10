fc6040622be5c50d713d72d8f5f3d6bc3e186c80
Rootul Patel <rootulp@gmail.com>
2025-10-10 10:36:49 -0400

    refactor: no op logger

--- store/iavl/store.go
+++ store/iavl/store.go
@@ -52,8 +52,10 @@ func LoadStore(db dbm.DB, logger log.Logger, key types.StoreKey, id types.Commit
 // provided DB. An error is returned if the version fails to load, or if called with a positive
 // version on an empty tree.
 func LoadStoreWithInitialVersion(db dbm.DB, logger log.Logger, key types.StoreKey, id types.CommitID, initialVersion uint64, cacheSize int, disableFastNode bool, metrics metrics.StoreMetrics) (types.CommitKVStore, error) {
-	no_op_logger := log.NewNopLogger()
-	tree := iavl.NewMutableTree(wrapper.NewDBWrapper(db), cacheSize, disableFastNode, no_op_logger, iavl.InitialVersionOption(initialVersion))
+	// The IAVL debug logs are too noisy so this uses a no-op logger to silence all
+	// iavl logs.
+	nopLogger := log.NewNopLogger()
+	tree := iavl.NewMutableTree(wrapper.NewDBWrapper(db), cacheSize, disableFastNode, nopLogger, iavl.InitialVersionOption(initialVersion))
 
 	isUpgradeable, err := tree.IsUpgradeable()
 	if err != nil {
